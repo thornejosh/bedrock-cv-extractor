9c441f990f8a88d65703210d2326907f04fdf0d5
Josh Thorne <josh.thorne@motorway.co.uk>
2025-01-28 22:13:28 +0200

    Initial commit

---
+++ test/bedrock-document.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BedrockDocument from '../lib/bedrock-document-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bedrock-document-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BedrockDocument.BedrockDocumentStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
